02afbb612046601d0e6a750d281b30318d98a95b
Mohammed Gharbi <simo163@hotmail.co.uk>
2016-04-12 18:09:17 +0200

    fixing the print invoice settings edit page

--- app/overrides/add_print_invoice_to_admin_configuration_sidebar.rb
+++ app/overrides/add_print_invoice_to_admin_configuration_sidebar.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
-  virtual_path:  'spree/admin/shared/sub_menu/_configuration',
+  virtual_path:  'spree/admin/shared/_store_tabs',
   name:          'print_invoice_admin_configurations_menu',
-  insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-  text:          '<%= configurations_sidebar_menu_item Spree.t(:settings, scope: :print_invoice),
-                      spree.edit_admin_print_invoice_settings_path %>'
+  insert_bottom: '[data-hook="admin_settings_store_tabs"]',
+  text:          '<%= settings_tab_item Spree.t(:settings, scope: :print_invoice), edit_admin_print_invoice_settings_path %>',
+  original:      '4a76d47bc518a3b46ff202d6fbeae815be4d3432'
 )
