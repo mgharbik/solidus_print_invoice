02b9746cebe75bb0553dd4f19100470cae524d2b
Mohammed Gharbi <simo163@hotmail.co.uk>
2016-04-12 10:53:04 +0200

    fixing documents link in the order tabs

--- app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
+++ app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(
-  virtual_path: 'spree/admin/shared/_order_tabs',
+  virtual_path: 'spree/admin/shared/_order_submenu',
   name:         'print_invoice_order_tab_links',
   insert_bottom: '[data-hook="admin_order_tabs"]',
   partial:      'spree/admin/orders/print_invoice_order_tab_links'
