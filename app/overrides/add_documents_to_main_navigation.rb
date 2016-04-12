50390c528207055233e4fcaf6135b89c98a6df49
Mohammed Gharbi <simo163@hotmail.co.uk>
2016-04-12 16:43:53 +0200

    fixing documents link in the menu tabs

--- app/overrides/add_documents_to_main_navigation.rb
+++ app/overrides/add_documents_to_main_navigation.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
-  virtual_path:  'spree/layouts/admin',
-  insert_bottom: '#main-sidebar',
-  partial:       'spree/admin/shared/menu/documents_tab',
-  name:          'documents_tab'
+  virtual_path:  'spree/admin/shared/_menu',
+  name:          'add_admin_document_tab',
+  insert_bottom: '[data-hook="admin_tabs"]',
+  partial:       'spree/admin/shared/document_tab',
+  original:      'b7b242ee058673679949d7e3050be5974938294d'
 )
--- app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
+++ app/overrides/add_print_invoice_links_to_admin_order_tabs.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
-  virtual_path: 'spree/admin/shared/_order_submenu',
-  name:         'print_invoice_order_tab_links',
+  virtual_path:  'spree/admin/shared/_order_submenu',
+  name:          'print_invoice_order_tab_links',
   insert_bottom: '[data-hook="admin_order_tabs"]',
-  partial:      'spree/admin/orders/print_invoice_order_tab_links'
+  partial:       'spree/admin/orders/print_invoice_order_tab_links',
+  original:      '29164565320a8b59ef605fb38b226d152999e0db'
 )
